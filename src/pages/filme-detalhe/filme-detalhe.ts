65750b1ec4b40302b49b8e89e5c4440823257955
Luciene <lfnascimento99@gmail.com>
2019-04-07 12:58:04 -0300

    third commit

--- src/pages/filme-detalhe/filme-detalhe.ts
+++ src/pages/filme-detalhe/filme-detalhe.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import firebase from 'firebase';
 import { Filme } from '../../model/filme';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Post } from '../../model/post';
 
 @IonicPage()
 @Component({
@@ -11,12 +13,29 @@ import { Filme } from '../../model/filme';
 export class FilmeDetalhePage {
 
   filme: Filme;
+
+  post: Post[] = [];
   
   constructor(public navCtrl: NavController,
     public navParams: NavParams) {
 
     this.filme = this.navParams.get('filme');
-
   }
+
+  getPost(){
+
+    var postRef = firebase.firestore().collection("post");
+
+    postRef.get().then(query => {
+      query.forEach(doc => {
+        let p = new Filme();
+        p.id = doc.id;
+        p.setDados(doc.data());
+        this.post.push();
+      });
+    });
+    
+  }
+    
  
   }
